e6148cef2fe4a241eb6c37b38837de659b486c0f
guaaan <juampablo.txt@gmail.com>
2021-09-28 16:25:16 -0300

    lista todo conectada y terminada sin estilos

--- reactapi/src/App.js
+++ reactapi/src/App.js
@@ -1,5 +1,7 @@
-import React, {useEffect, useState} from 'react';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
